d7f29d479c4fb68bf6b2ea11411b4c246d65e390
geordieable <azong86@gmail.com>
2026-05-25 15:26:45 +0800

    Update Meituan configuration for new developer mode

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
-// 美团开放平台配置 - 请替换为你自己的
-module.exports = {
-  APP_KEY: "你的美团AppKey",
-  APP_SECRET: "你的美团AppSecret",
+// 美团新版个人开发者 - 仅TOKEN模式
+module。exports = {
   API_HOST: "https://openapi.meituan.com",
+  // 在这里填入你后台的 Token
+  MEITUAN_TOKEN: "4a710fa7ca107e26a1b26b9a77b74420d6a0cb9b5fe8231137e1b01d54e7f5fd",
 
-  // 缓存时长（秒）
+  // 缓存时长
   CACHE_SHOP: 3600,
   CACHE_GROUP: 1800,
   CACHE_RANK: 7200
