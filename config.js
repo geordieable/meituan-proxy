3480b3fd0c84048815442883c43f3b5b83417b57
geordieable <azong86@gmail.com>
2026-05-25 15:37:47 +0800

    Add files via upload

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // 美团新版个人开发者 - 仅TOKEN模式
-module。exports = {
+module.exports = {
   API_HOST: "https://openapi.meituan.com",
   // 在这里填入你后台的 Token
   MEITUAN_TOKEN: "4a710fa7ca107e26a1b26b9a77b74420d6a0cb9b5fe8231137e1b01d54e7f5fd",
